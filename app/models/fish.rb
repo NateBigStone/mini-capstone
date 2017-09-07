90b49b7771589367953c024823f27941f898ac9c
NateBigStone <n.gaut@bigstonemgmt.com>
2017-09-06 21:38:34 -0500

    Fish Price Update

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,2 +1,15 @@
 class Fish < ApplicationRecord
+  def sale_message
+    if price < 2 
+      return " -Discount Item!!!" 
+    else
+      " -Everyday Value!"
+    end
+  end  
+  def tax
+    return price * 0.09 
+  end
+  def total
+    return price + tax
+  end
 end
